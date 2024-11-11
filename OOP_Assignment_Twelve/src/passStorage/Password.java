02414d497bbc6ba8bfae405fd9dc5c6ecbe6dbbb
jackd <jackd@Jacks_PC.attlocal.net>
2024-11-11 17:17:58 -0500

    Creation of classes

---
+++ OOP_Assignment_Twelve/src/passStorage/Password.java
@@ -0,0 +1,8 @@
+package passStorage;
+
+public class Password {
+	
+	private char[] pass;
+	private int indx;
+	
+}
---
+++ OOP_Assignment_Twelve/src/passwordCheck/CheckPassword.java
@@ -0,0 +1,10 @@
+package passwordCheck;
+
+public class CheckPassword {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
